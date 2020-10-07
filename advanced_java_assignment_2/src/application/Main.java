00d3d8ab13e20dc3fd65d3e2e13e8655099ccac5
Sharjeel Ahmed <msharjeel.irshad@gmail.com>
2020-10-07 19:33:28 +1100

    Added Title and Label

--- advanced_java_assignment_2/src/application/Main.java
+++ advanced_java_assignment_2/src/application/Main.java
@@ -3,15 +3,29 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Set title for the stage 
+			primaryStage.setTitle("Test"); 
+				  
+			// Create a label 
+			Label name = new Label("Sharjeel");
+						
+			// Create a Stack Pane
+			 StackPane sp = new StackPane();
+				        
+			// Add field
+			sp.getChildren().add(name);
+			
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(sp,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
